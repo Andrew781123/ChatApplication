0c8607b523bf22f55c74b18a072c9d2e5f8ca0a3
Andrew781123 <leechihoandrew1123@gmail.com>
2021-05-29 23:51:31 +0800

    image-picker setup

--- App.tsx
+++ App.tsx
@@ -9,12 +9,33 @@
  */
 
 import React from 'react';
-import {SafeAreaView, Text} from 'react-native';
+import {Button, SafeAreaView, Text} from 'react-native';
+import {
+  Callback,
+  ImageLibraryOptions,
+  launchImageLibrary,
+  ImagePickerResponse,
+} from 'react-native-image-picker';
 
 const App = () => {
+  const imageLibOptions: ImageLibraryOptions = {
+    mediaType: 'photo',
+  };
+
+  const callBack: Callback = (response: ImagePickerResponse) => {
+    if (response.didCancel) {
+      console.log('User cancelled');
+    }
+  };
+
+  const openGallery = () => {
+    launchImageLibrary(imageLibOptions, callBack);
+  };
+
   return (
     <SafeAreaView>
       <Text>Hello World</Text>
+      <Button onPress={openGallery} title="Select image" />
     </SafeAreaView>
   );
 };
